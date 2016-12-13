822f78d073b58daf224c8dbbcc18dbeec7b2868a
tosFa <hoti.fatos@gmail.com>
2016-12-14 00:28:10 +0100

    fixed a bug with setupFunctions added the base route component for confirmation

---
+++ src/common/components/routes/Confirmation/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Confirmation extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>You have successfully confirmed your email</h1>
+      </div>
+    );
+  }
+}
